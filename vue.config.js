9717014d4337ab3b02afdf8678d9c7d1bdf345d2
leehyunjun <msh794000@gmail.com>
2022-11-03 15:57:43 +0900

    수정

--- vue.config.js
+++ vue.config.js
@@ -1,18 +1,3 @@
-// const path = require('path')
-
-// module.exports = {
-//     publicPath: '',
-//     // outputDir: path.resolve("../back/views"),
-// //     devServer: {
-// //         "proxy": {
-// //             "/api": {
-// //                 "target": "http://localhost:8001",
-// //                 "pathRewrite": { '^/': '' },
-// //                 "ws": true,
-// //                 "changeOrigin": true
-// //             },
-// //         },
-
-
-// //     },
-// }
+module.exports = {
+publicPath: ''
+};
