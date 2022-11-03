70ec033b496badf81aaf7976c55c281466d04424
leehyunjun <msh794000@gmail.com>
2022-11-03 14:35:37 +0900

    수정 시발 카페24

--- vue.config.js
+++ vue.config.js
@@ -1,18 +1,18 @@
-const path = require('path')
+// const path = require('path')
 
-module.exports = {
-    publicPath: '',
-    outputDir: path.resolve("../back/views"),
-    devServer: {
-        "proxy": {
-            "/api": {
-                "target": "http://localhost:8001",
-                "pathRewrite": { '^/': '' },
-                "ws": true,
-                "changeOrigin": true
-            },
-        },
+// module.exports = {
+//     publicPath: '',
+//     // outputDir: path.resolve("../back/views"),
+// //     devServer: {
+// //         "proxy": {
+// //             "/api": {
+// //                 "target": "http://localhost:8001",
+// //                 "pathRewrite": { '^/': '' },
+// //                 "ws": true,
+// //                 "changeOrigin": true
+// //             },
+// //         },
 
 
-    },
-}
+// //     },
+// }
