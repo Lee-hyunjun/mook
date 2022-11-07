5fb3d765b6009129456593161b470cd004eb59a6
leehyunjun <msh794000@gmail.com>
2022-11-07 16:45:37 +0900

    수정

--- src/components/About/About.vue
+++ src/components/About/About.vue
@@ -14,17 +14,23 @@
     </div>
 
     <div id="item1">
-      <h1>SEOUL <br>LOTTE TOWER</h1>
       <div class="img"></div>
-    </div>
-
-    <div id="item2">
-      <h1>CITY POP<br>NEUKKIM ITNE</h1>
-      <div class="box">
-        <span>JP CITY</span> 
+      <div class="text">
+        <p class="aboutUs"></p>
+        <span></span>
       </div>
     </div>
 
+    <div id="item2">
+      <div class="img"></div>
+      <div class="text">
+        <p class="aboutUs"></p>
+        <span></span>
+      </div>
+    </div>
+
+    <div id="item3">
+    </div>
 
     <!-- <div id="item1">
       <h1>WOW CITY POP<br></h1>
@@ -103,9 +109,6 @@ export default {
         }
     }
 
-    #item2{
-        animation: fadeInRight 2s;
-    }
 
 
 
@@ -131,116 +134,40 @@ export default {
     }
 
     #item1{
-      margin-left: 50%;
-      margin-top: 18vh;
-      width: 40vw;
-      height: 45vh;
-      position: relative;
-      // overflow: hidden;
       display: flex;
-      align-items: center;
-      justify-content: center;
+      justify-content: flex-end;
+      align-items: flex-end;
+      width: 100%;
+      height: 80vh;
+      z-index: 1;
+      position: relative;
       >.img{
-        background-image: url('https://cdn.jsdelivr.net/gh/Lee-hyunjun/mook@main/src/assets/aboutImg1.jpg');
+        width: 20%;
+        height: 80%;
         background-size: cover;
-        position: relative;
-        width: 75%;
-        height: 75%;
-      }
-     
-      >h1{
-        position: absolute;
-        color: white;
-        font-size: 4.8vw;
-        width: 110%;
-        letter-spacing : -.2vw;
-        z-index: 1;
-        line-height: 5.5vw;
-        top: 12%;
-        right: 40%;
+        background-image: url("https://cdn.jsdelivr.net/gh/Lee-hyunjun/mook@main/src/assets/about/content1.png");
       }
     }
-
-    #item1::before{
-      position: absolute;
-      top: 0;
-      left: 0;
-      right: 0;
-      bottom: 0;
-      width: 100%;
-      height: 100%;
-      overflow: hidden;
-      -webkit-filter: blur(3px); 
-      -moz-filter: blur(3px); 
-      -o-filter: blur(3px); 
-      -ms-filter: blur(3px); 
-      filter: blur(3px);
-      transform: scale(1.02);
-      background-size: cover;
-      background-image: url('https://cdn.jsdelivr.net/gh/Lee-hyunjun/mook@main/src/assets/aboutImg1.jpg');
-      z-index: -1; 
-      content: "";
-    }
-
     #item2{
-      margin-left: 10%;
-      margin-top: 15vh;
-      width: 40vw;
-      height: 50vh;
+      width: 100%;
+      height: 80vh;
+      z-index: 1;
       position: relative;
-      // overflow: hidden;
-      display: flex;
-      align-items: center;
-      justify-content: center;
-      >.box{
-        // background-image: url('../../assets/aboutImg2.png');
-        // background-size: cover;
-        position: relative;
-        border: 6px solid white;
-        width: 80%;
-        height: 75%;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        >span{
-          font-family: BebasNeue-Regular;
-          display: block;
-          background: white;
-          font-size: 3vw;
-          font-weight: bold;
-          color: black;
-          padding: 1.5% 7% .5% 7%;
-        }
-      }
-     
-      >h1{
-        position: absolute;
-        color: white;
-        font-size: 5vw;
-        width: 110%;
-        letter-spacing : -.2vw;
-        z-index: 1;
-        line-height: 5.5vw;
-        text-align: right;
-        bottom: 10%;
-        right: -57%;
+      >.img{
+        width: 30%;
+        height: 85%;
+        background-size: cover;
+        background-image: url("https://cdn.jsdelivr.net/gh/Lee-hyunjun/mook@main/src/assets/about/content2.png");
       }
     }
 
-    #item2::before{
-      position: absolute;
-      top: 0;
-      left: 0;
-      right: 0;
-      bottom: 0;
+    #item3{
       width: 100%;
-      height: 100%;
-      overflow: hidden;
+      height: 88vh;
       background-size: cover;
-      background-image: url('https://cdn.jsdelivr.net/gh/Lee-hyunjun/mook@main/src/assets/aboutImg2.png');
-      z-index: -1; 
-      content: "";
+      background-image: url("https://cdn.jsdelivr.net/gh/Lee-hyunjun/mook@main/src/assets/about/cmpyGroup.png");
     }
+
   }
 
   .About::before{
